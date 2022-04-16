0e8af5deaea8d4926613ef0a087b1decff79bdf6
Anusha.Haz <63346348+AnushaHazarika@users.noreply.github.com>
2022-04-16 11:27:57 +0530

    Header created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 // import logo from './logo.svg';
+import React from 'react';
+import {CssBaseline, Grid} from '@material-ui/core';
+
 import './App.css';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Heyaa</h2>
-    </div>
+    <>
+      <CssBaseline/>
+      <Header/>
+      <Grid container spacing={3} style={{width : '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List/>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map/>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
